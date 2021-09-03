828e4326ceba51f6ea6539774030fec3dc8b09df
Tyler Stinnett <ts399320@ohio.edu>
2021-09-03 15:43:49 -0400

    lab prog commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Tyler Stinnett
+ *        Date: 9/03/21
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
